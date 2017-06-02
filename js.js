1617c0d8e09bdea2ac6d7d3d355e36d313cf53ae
dvikster <dvikster@gmail.com>
2017-06-02 18:00:01 +0300

    added  files

--- js.js
+++ js.js
@@ -19,4 +19,37 @@ document.getElementById('rezult').onclick = function rezultF() {
         if((c<a)&&(a>b)){document.getElementById('out').innerHTML = `максимальное число: ${a}`;}
     }
     rezult();
-}
+};
+
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+let color = document.getElementById('color');
+canvas.onmousedown = function(event) {
+    canvas.onmousemove = function (event) {
+        ctx.fillStyle = color.value;
+        ctx.fillRect(event.offsetX, event.offsetY, 10, 10);
+    };
+    canvas.onmouseup = function(event) {
+        canvas.onmousemove = function (event) {return false};
+        ctx.fillStyle = color.value;
+        ctx.fillRect(event.offsetX, event.offsetY, 10, 10);
+    };
+};
+
+
+document.getElementById('clear').onclick = function () {
+    ctx.clearRect(0,0,600,600);
+};
+
+
+
+// ctx.fillStyle='#2bbbad';
+// ctx.beginPath();
+// ctx.beginPath();
+// ctx.strokeStyle='#ee6e73';
+// ctx.lineWidth = '2';
+// ctx.moveTo(0,300);//начало
+// ctx.lineTo(600,300);
+// ctx.stroke();
+// ctx.fillRect(2*x+300,2*y+300, 2,2);
+
